c7e4882dda5792cc8e7e7c3e08d12d62b10e558b
Chen Yicai <yicai.cyj@taobao.com>
2015-07-08 17:27:20 +0800

    Use sync methods for test code cleaness

--- test/test.compileAll.js
+++ test/test.compileAll.js
@@ -1,40 +1,17 @@
 'use strict'
 
-var Promise = require('native-or-bluebird')
-var glob = require('glob')
+var glob = require('glob').sync
 var fs = require('fs')
 var path = require('path')
 var expect = require('expect.js')
 
 var compileAll = require('..').compileAll
 
-
-function readFileAsync(fpath, encoding) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(fpath, encoding, function(err, content) {
-      if (err) reject(err)
-      else resolve(content)
-    })
-  })
-}
-
-function globAsync(pattern) {
-  return new Promise(function(resolve, reject) {
-    glob(pattern, function(err, entries) {
-      if (err) reject(err)
-      else resolve(entries)
-    })
-  })
-}
+var readFile = fs.readFileSync
 
 
 describe('compileAll', function() {
   var cwd = path.join(__dirname, 'example')
-
-  function readPackage(name) {
-    return readFileAsync(path.join(cwd, 'node_modules', name, 'package.json'), 'utf-8')
-      .then(JSON.parse)
-  }
 
   it('should compile matched modules', function(done) {
     var modules = ['inherits', 'heredoc']
@@ -45,16 +22,12 @@ describe('compileAll', function() {
       match: '{' + modules.join(',') + '}'
     })
       .then(function() {
-        return Promise.all([
-          globAsync(path.join(cwd, 'public/**/*.js')),
-          Promise.all(modules.map(readPackage))
-        ])
-      })
-      .then(function(results) {
-        var entries = results[0].map(function(entry) {
+        var entries = glob(path.join(cwd, 'public/**/*.js')).map(function(entry) {
           return path.relative(path.join(cwd, 'public'), entry)
         })
-        var packages = results[1]
+        var packages = modules.map(function(name) {
+          return JSON.parse(readFile(path.join(cwd, 'node_modules', name, 'package.json'), 'utf-8'))
+        })
 
         packages.forEach(function(pkg) {
           var id = path.join(pkg.name, pkg.version, pkg.main || 'index.js')
@@ -71,14 +44,10 @@ describe('compileAll', function() {
       base: 'components'
     })
       .then(function() {
-        return globAsync(path.join(cwd, 'public/**/*.js'))
-      })
-      .then(function(entries) {
-        return entries.map(function(entry) {
+        var entries = glob(path.join(cwd, 'public/**/*.js')).map(function(entry) {
           return path.relative(cwd, entry)
         })
-      })
-      .then(function(entries) {
+
         expect(entries.indexOf('public/ma/nga.js') >= 0).to.be(true)
         done()
       })
