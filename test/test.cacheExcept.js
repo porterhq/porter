6f071150afa671300b9d1daa898fa5ef51dc320e
Chen Yicai <yicai.cyj@taobao.com>
2016-01-07 21:52:05 +0800

    Fix: ez-editor@0.2.6 & oceanify["import"]

--- test/test.cacheExcept.js
+++ test/test.cacheExcept.js
@@ -26,7 +26,7 @@ describe('oceanify cacheExcept', function() {
   it('should skip compilation if within cache exceptions', function* () {
     yield new Promise(function(resolve, reject) {
       request(app.callback())
-        .get('/ez-editor/0.2.4/index.js')
+        .get('/ez-editor/0.2.6/index.js')
         .expect(200)
         .end(function(err) {
           if (err) reject(err)
@@ -36,7 +36,7 @@ describe('oceanify cacheExcept', function() {
 
     yield sleep(1)
 
-    var fpath = path.join(__dirname, 'example/public/ez-editor/0.2.4/index.js')
+    var fpath = path.join(__dirname, 'example/public/ez-editor/0.2.6/index.js')
     expect(yield exists(fpath)).to.be(false)
   })
 })
