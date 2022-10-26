04a23a996d792c1b9af8dc253b915f822367fcdc
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2022-10-26 10:05:04 +0800

    refactor: simplify bundle iterate order, make sure descendents are bundled before ancestors (#181)

---
+++ packages/porter/test/unit/ts_module.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { strict: assert } = require('assert');
+const path = require('path');
+const fs = require('fs/promises');
+const Porter = require('../..');
+
+describe('TsModule', function() {
+  const root = path.resolve(__dirname, '../../../demo-typescript');
+  let porter;
+
+  before(async function() {
+    await fs.rm(path.join(root, 'public'), { recursive: true, force: true });
+    porter = new Porter({
+      root,
+      entries: [ 'app.tsx' ],
+    });
+    await porter.ready();
+  });
+
+  after(async function() {
+    await porter.destroy();
+  });
+
+  describe('module.load()', function() {
+    it('need to neglect type imports in advance', async function() {
+      const mod = porter.packet.files['app.tsx'];
+      assert.deepEqual(mod.dynamicImports, ['./utils/math']);
+      assert.deepEqual(mod.imports, ['react', 'react-dom', 'prismjs', './home']);
+    });
+  });
+});
