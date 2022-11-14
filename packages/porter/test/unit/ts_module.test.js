9b50db64840caa758b04ec33d91aa15b3f825fcc
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2022-11-14 17:21:47 +0800

    fix: ts module source map (#195)

--- packages/porter/test/unit/ts_module.test.js
+++ packages/porter/test/unit/ts_module.test.js
@@ -2,7 +2,9 @@
 
 const { strict: assert } = require('assert');
 const path = require('path');
+// const fs = require('fs/promises');
 const Porter = require('../..');
+const { SourceMapConsumer } = require('source-map');
 
 describe('TsModule', function() {
   const root = path.resolve(__dirname, '../../../../examples/typescript');
@@ -26,6 +28,30 @@ describe('TsModule', function() {
       const mod = porter.packet.files['app.tsx'];
       assert.deepEqual(mod.dynamicImports, ['./utils/math']);
       assert.deepEqual(mod.imports, ['react', 'react-dom', 'prismjs', 'lodash', './home']);
+    });
+
+    it('should return loaded source rather than the traspiled one', async function() {
+      const mod = porter.packet.files['app.tsx'];
+      const result = await mod.load();
+      assert(result.code.includes('import React'));
+    });
+  });
+
+  describe('module.transpile()', async function() {
+    it('should generate correct source mappings', async function() {
+      const mod = porter.packet.files['app.tsx'];
+      const result = await mod.transpile(await mod.load());
+      await SourceMapConsumer.with(JSON.stringify(result.map), null, async consumer => {
+        // ReactDOM.render(<App />, document.querySelector('#ReactApp'));
+        const mapping = consumer.generatedPositionFor({
+          source: 'porter:///components/app.tsx',
+          line: 34,
+          column: 0,
+        });
+        const line = result.code.split('\n')[mapping.line - 1];
+        assert.ok(line);
+        assert.ok(line.includes("document.querySelector('#ReactApp')"));
+      });
     });
   });
 });
